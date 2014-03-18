cac6bfd6e7a5477c3233b3d93eb5ad31d8e14101
Peter Brown <phbrown@acm.org>
2014-03-18 15:32:43 -0400

    I now know that SVG works.  Trying to fix the placement.

--- www/js/index.js
+++ www/js/index.js
@@ -38,11 +38,15 @@ var app = {
     // Update DOM on a Received Event
     receivedEvent: function(id) {
         var parentElement = document.getElementById(id);
-        var listeningElement = parentElement.querySelector('.listening');
-        var receivedElement = parentElement.querySelector('.received');
-
-        listeningElement.setAttribute('style', 'display:none;');
-        receivedElement.setAttribute('style', 'display:block;');
+        parentElement.setAttribute('style', 'display: none');
+        var svgElt = document.getElementById('svgdiv');
+        svgElt.setAttribute('style', 'display: block');
+        
+        //~ var listeningElement = parentElement.querySelector('.listening');
+        //~ var receivedElement = parentElement.querySelector('.received');
+//~ 
+        //~ listeningElement.setAttribute('style', 'display:none;');
+        //~ receivedElement.setAttribute('style', 'display:block;');
 
         console.log('Received Event: ' + id);
     }
